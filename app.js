852347738d0eee00e1ab3dc5250a65a1ff52fd23
Callum Woon <callumwoon@Callums-MacBook-Pro-2.local>
2020-05-25 17:40:27 +0100

    implemented counter

---
+++ app.js
@@ -0,0 +1,8 @@
+let seconds = 0
+
+function incrementSeconds() {
+    seconds += 1;
+    document.getElementById('clock').innerHTML = seconds;
+}
+
+ const interval = setInterval(incrementSeconds, 1000);
