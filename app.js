ad3ef497590b1f917be1365d250642791f7480fd
Callum Woon <callumwoon@Callums-MacBook-Pro-2.local>
2020-05-26 15:59:18 +0100

    start button fix

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const startButton = document.getElementById('startButton')
 
 let seconds = 0
 
+let isRunning = false;
+
 startButton.addEventListener("click", function() {
+    if (isRunning === false) {
     setInterval(function () {
     seconds++;
     clock.textContent = seconds;
     }, 1000);
+    isRunning = true;
+    }
 });
