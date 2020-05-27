4745e26b8835290d4e6cd9a6747316e8b3bcc3be
Callum Woon <callumwoon@Callums-MacBook-Pro-2.local>
2020-05-27 11:44:44 +0100

    added lap button

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const clock = document.getElementById('clock')
-const startButton = document.getElementById('startButton')
-const stopButton = document.getElementById('stopButton')
+// const clock = document.getElementById('clock')
+// const startButton = document.getElementById('startButton')
+// const stopButton = document.getElementById('stopButton')
+// const resetButton = document.getElementById('resetButton')
+// const lapButton = document.getElementById('lapButton')
+
+// WHY IS THE CODE ABOVE NOT NEEDED?
 
 let seconds = 0
 
@@ -25,4 +29,12 @@ stopButton.addEventListener("click", () => {
 resetButton.addEventListener("click", () => {
     clock.textContent = 0;
     seconds = 0;
+    lapsUl.textContent = '';
 });
+
+lapButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0]; // AM I CORRECT IN THINKING IT HAS TO BE TAG NAME AS THIS IS A DOM PROCESS RATHER THAN HTML?
+    let li = document.createElement('li');
+    li.textContent = seconds;
+    ul.appendChild(li);
+  });
