b1f8dabe9e0fa2ee1a2ab3144beb1b36cfa55cfa
Callum Woon <callumwoon@Callums-MacBook-Pro-2.local>
2020-05-27 16:55:23 +0100

    lap button wip

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-// const clock = document.getElementById('clock')
+// const mainClock = document.getElementById('mainClock')
 // const startButton = document.getElementById('startButton')
 // const stopButton = document.getElementById('stopButton')
 // const resetButton = document.getElementById('resetButton')
 // const lapButton = document.getElementById('lapButton')
-
-// WHY IS THE CODE ABOVE NOT NEEDED?
 
 let seconds = 0
 
 let isRunning = false;
 let intervalID;
 
+
 startButton.addEventListener("click", () => {
     if (isRunning === false) {
         intervalID = setInterval(() => {
-        seconds++;
-        clock.textContent = seconds;
-    }, 1000);
-    isRunning = true;
+            seconds++;
+            mainClock.textContent = seconds;
+        }, 1000);
+        addRow('lapTable', 'here');
+        isRunning = true;
     }
 });
 
 stopButton.addEventListener("click", () => {
-        clearInterval(intervalID);
-        isRunning = false;
+    clearInterval(intervalID);
+    isRunning = false;
 });
+
 
 resetButton.addEventListener("click", () => {
-    clock.textContent = 0;
+    mainClock.textContent = 0;
     seconds = 0;
-    lapsUl.textContent = '';
+    lapNumber = 0;
+    lapTable.textContent = '';
 });
 
+let lapNumber = 0;
+
+function addRow(tableID, lapTime) {
+    let tableRef = document.getElementById(tableID);
+    let newRow = tableRef.insertRow(0);
+    let newFirstCell = newRow.insertCell(0);
+    let newSecondCell = newRow.insertCell(1);
+    let newFirstText = document.createTextNode('Lap ' + lapNumber + ':');
+    let newSecondText = document.createTextNode(lapTime);
+    newFirstCell.appendChild(newFirstText);
+    newSecondCell.appendChild(newSecondText);
+  }
+
 lapButton.addEventListener('click', () => {
-    let ul = document.getElementsByTagName('ul')[0]; // AM I CORRECT IN THINKING IT HAS TO BE TAG NAME AS THIS IS A DOM PROCESS RATHER THAN HTML?
-    let li = document.createElement('li');
-    li.textContent = seconds;
-    ul.appendChild(li);
-  });
+    if (lapNumber != 0) {
+        addRow('lapTable', '');
+    } else {
+        addRow('lapTable', seconds);
+    }
+    lapNumber++;
+});
